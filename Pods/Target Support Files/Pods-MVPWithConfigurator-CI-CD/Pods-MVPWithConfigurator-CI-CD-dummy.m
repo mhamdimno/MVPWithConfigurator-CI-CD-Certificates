3d2aad43f5eb649b462ff81c9ec964f1cd78f53b
Mhamdino95 <mhamdino95@gmail.com>
2021-10-06 17:27:40 +0200

    init podfile and gem and ruby version

---
+++ Pods/Target Support Files/Pods-MVPWithConfigurator-CI-CD/Pods-MVPWithConfigurator-CI-CD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVPWithConfigurator_CI_CD : NSObject
+@end
+@implementation PodsDummy_Pods_MVPWithConfigurator_CI_CD
+@end
---
+++ Pods/Target Support Files/Pods-MVPWithConfigurator-CI-CD/Pods-MVPWithConfigurator-CI-CD-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MVPWithConfigurator_CI_CDVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MVPWithConfigurator_CI_CDVersionString[];
+
